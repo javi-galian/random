138fc40571905135ccb604162010056f4f5e2de5
javi-galian <javi.galian@gmail.com>
2015-06-15 22:52:19 +0200

    Modificaciones

---
+++ README.md
@@ -0,0 +1,4 @@
+<p>Este proyecto es para el curso de Miriada-X (Desarrollo de servicios en la nube con HTML5, Javascript y node.js)</b>
+<p>www.miridadax.net</p>
+<p>Se ejecuta con <code>node random.js</code></p>
+<p>Consiste en generar un número aleatorio entre 0 y 1 y decir si es mayor o menor de 0.5</p>
--- random.js
+++ random.js
@@ -1,10 +1,8 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
+// Fichero original de github.com/jquemada/random
+
 var numero = Math.random();
 
-var str =' MAYOR que 0,5';
-
-if (numero <= 0.5){
-  str = ' MENOR que 0,5';
-}
+var str = (numero > 0.5 ? " MAYOR que 0.5" : " MENOR que 0.5");
 
 console.log('\n' + numero + str + '\n');
